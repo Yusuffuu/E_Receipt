b981c0cefad8940d6cd0fca4f75b3331212c7e22
Yusuf <karanjajose003@gmail.com>
2026-06-02 14:49:46 +0300

    Update portfolio with latest changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/E_Receipt/',
+  css: {
+    postcss: './postcss.config.js',
+  },
 })
