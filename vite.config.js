bb28c95d019bd981e7323e0672189361a8be7c8d
Yusuf <166618525+Yusuffuu@users.noreply.github.com>
2026-06-17 08:05:35 +0300

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/E_Receipt/',
+  base: '/',
   // Ensure public assets are copied
   publicDir: 'public',
 })
